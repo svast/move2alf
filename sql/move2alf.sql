b388d335617f9407bb90080398330186ac606e6b
Dirk Ooms <dirk@onesparrow.com>
2010-12-07 13:19:53 +0000

    first version move2alf database
    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@438 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ sql/move2alf.sql
@@ -0,0 +1,483 @@
+-- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: move2alf
+-- ------------------------------------------------------
+-- Server version	5.1.41-3ubuntu12.7
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `action`
+--
+
+DROP TABLE IF EXISTS `action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `action` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `action`
+--
+
+LOCK TABLES `action` WRITE;
+/*!40000 ALTER TABLE `action` DISABLE KEYS */;
+/*!40000 ALTER TABLE `action` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `actionConfigPropertyDefinition`
+--
+
+DROP TABLE IF EXISTS `actionConfigPropertyDefinition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actionConfigPropertyDefinition` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `actionId` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `defaultValue` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actionConfigPropertyDefinition`
+--
+
+LOCK TABLES `actionConfigPropertyDefinition` WRITE;
+/*!40000 ALTER TABLE `actionConfigPropertyDefinition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actionConfigPropertyDefinition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `actionInputPropertyDefinition`
+--
+
+DROP TABLE IF EXISTS `actionInputPropertyDefinition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actionInputPropertyDefinition` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `actionId` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `defaultValue` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actionInputPropertyDefinition`
+--
+
+LOCK TABLES `actionInputPropertyDefinition` WRITE;
+/*!40000 ALTER TABLE `actionInputPropertyDefinition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actionInputPropertyDefinition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `actionReportPropertyDefinition`
+--
+
+DROP TABLE IF EXISTS `actionReportPropertyDefinition`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actionReportPropertyDefinition` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `actionId` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `defaultValue` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actionReportPropertyDefinition`
+--
+
+LOCK TABLES `actionReportPropertyDefinition` WRITE;
+/*!40000 ALTER TABLE `actionReportPropertyDefinition` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actionReportPropertyDefinition` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `configuredAction`
+--
+
+DROP TABLE IF EXISTS `configuredAction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `configuredAction` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `actionId` int(10) unsigned NOT NULL,
+  `appliedConfiguredActionOnSuccessId` int(10) unsigned DEFAULT NULL,
+  `appliedConfiguredActionOnFailureId` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `configuredAction`
+--
+
+LOCK TABLES `configuredAction` WRITE;
+/*!40000 ALTER TABLE `configuredAction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `configuredAction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `configuredActionProperty`
+--
+
+DROP TABLE IF EXISTS `configuredActionProperty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `configuredActionProperty` (
+  `configuredActionId` int(10) unsigned NOT NULL,
+  `actionConfigPropertyDefinitionId` int(10) unsigned NOT NULL,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY (`configuredActionId`,`actionConfigPropertyDefinitionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `configuredActionProperty`
+--
+
+LOCK TABLES `configuredActionProperty` WRITE;
+/*!40000 ALTER TABLE `configuredActionProperty` DISABLE KEYS */;
+/*!40000 ALTER TABLE `configuredActionProperty` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cycle`
+--
+
+DROP TABLE IF EXISTS `cycle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cycle` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `scheduleId` int(10) unsigned NOT NULL,
+  `startDateTime` datetime NOT NULL,
+  `endDateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cycle`
+--
+
+LOCK TABLES `cycle` WRITE;
+/*!40000 ALTER TABLE `cycle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cycle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `destination`
+--
+
+DROP TABLE IF EXISTS `destination`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `destination` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `user` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `communicationType` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `destination`
+--
+
+LOCK TABLES `destination` WRITE;
+/*!40000 ALTER TABLE `destination` DISABLE KEYS */;
+/*!40000 ALTER TABLE `destination` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `job` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `creatorId` int(10) unsigned NOT NULL,
+  `creationDateTime` datetime NOT NULL,
+  `lastModifyDateTime` datetime NOT NULL,
+  `firstConfiguredActionId` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job`
+--
+
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `processedDocument`
+--
+
+DROP TABLE IF EXISTS `processedDocument`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `processedDocument` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cycleId` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `processingDateTime` datetime NOT NULL,
+  `status` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `processedDocument`
+--
+
+LOCK TABLES `processedDocument` WRITE;
+/*!40000 ALTER TABLE `processedDocument` DISABLE KEYS */;
+/*!40000 ALTER TABLE `processedDocument` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `processedDocumentProperty`
+--
+
+DROP TABLE IF EXISTS `processedDocumentProperty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `processedDocumentProperty` (
+  `processedDocumentId` int(10) unsigned NOT NULL,
+  `configuredActionId` int(10) unsigned NOT NULL,
+  `actionReportPropertyDefinitionId` int(10) unsigned NOT NULL,
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `processedDocumentProperty`
+--
+
+LOCK TABLES `processedDocumentProperty` WRITE;
+/*!40000 ALTER TABLE `processedDocumentProperty` DISABLE KEYS */;
+/*!40000 ALTER TABLE `processedDocumentProperty` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `report`
+--
+
+DROP TABLE IF EXISTS `report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `report` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `jobId` int(10) unsigned NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `emailList` varchar(255) DEFAULT NULL,
+  `class` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `report`
+--
+
+LOCK TABLES `report` WRITE;
+/*!40000 ALTER TABLE `report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reportActionProperty`
+--
+
+DROP TABLE IF EXISTS `reportActionProperty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reportActionProperty` (
+  `reportId` int(10) unsigned NOT NULL,
+  `configuredActionId` int(10) unsigned NOT NULL,
+  `actionReportPropertyDefinitionId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`configuredActionId`,`actionReportPropertyDefinitionId`,`reportId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reportActionProperty`
+--
+
+LOCK TABLES `reportActionProperty` WRITE;
+/*!40000 ALTER TABLE `reportActionProperty` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reportActionProperty` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `runningAction`
+--
+
+DROP TABLE IF EXISTS `runningAction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `runningAction` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cycleId` int(10) unsigned NOT NULL,
+  `configuredActionId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `runningAction`
+--
+
+LOCK TABLES `runningAction` WRITE;
+/*!40000 ALTER TABLE `runningAction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `runningAction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schedule` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `jobId` int(10) unsigned NOT NULL,
+  `creatorId` int(10) unsigned NOT NULL,
+  `creationDateTime` datetime NOT NULL,
+  `lastModifyDateTime` datetime NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `startDateTime` datetime NOT NULL,
+  `endDateTime` datetime DEFAULT NULL,
+  `quartzScheduling` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedule`
+--
+
+LOCK TABLES `schedule` WRITE;
+/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
+/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `threadPool`
+--
+
+DROP TABLE IF EXISTS `threadPool`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `threadPool` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `size` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `threadPool`
+--
+
+LOCK TABLES `threadPool` WRITE;
+/*!40000 ALTER TABLE `threadPool` DISABLE KEYS */;
+/*!40000 ALTER TABLE `threadPool` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userPswd`
+--
+
+DROP TABLE IF EXISTS `userPswd`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userPswd` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userName` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userPswd`
+--
+
+LOCK TABLES `userPswd` WRITE;
+/*!40000 ALTER TABLE `userPswd` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userPswd` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userRole`
+--
+
+DROP TABLE IF EXISTS `userRole`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userRole` (
+  `userName` varchar(30) NOT NULL,
+  `role` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userRole`
+--
+
+LOCK TABLES `userRole` WRITE;
+/*!40000 ALTER TABLE `userRole` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userRole` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-12-07 14:18:20
