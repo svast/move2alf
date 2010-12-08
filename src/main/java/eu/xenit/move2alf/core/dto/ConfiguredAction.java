4209d157c41f33b4d38bc974f398d8a2bce15743
Jonas Heylen <jheylen@xenit.eu>
2010-12-08 17:01:19 +0000

    More Hibernate config, dependencies and unit tests
    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@454 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/eu/xenit/move2alf/core/dto/ConfiguredAction.java
+++ src/main/java/eu/xenit/move2alf/core/dto/ConfiguredAction.java
@@ -3,5 +3,39 @@ package eu.xenit.move2alf.core.dto;
 import eu.xenit.move2alf.common.IdObject;
 
 public class ConfiguredAction extends IdObject {
+	private Action action;
+	
+	private ConfiguredAction appliedConfiguredActionOnSuccess;
+	
+	private ConfiguredAction appliedConfiguredActionOnFailure;
+	
+	public ConfiguredAction() {
+		
+	}
 
+	public void setAction(Action action) {
+		this.action = action;
+	}
+
+	public Action getAction() {
+		return action;
+	}
+
+	public void setAppliedConfiguredActionOnSuccess(
+			ConfiguredAction appliedConfiguredActionOnSuccess) {
+		this.appliedConfiguredActionOnSuccess = appliedConfiguredActionOnSuccess;
+	}
+
+	public ConfiguredAction getAppliedConfiguredActionOnSuccess() {
+		return appliedConfiguredActionOnSuccess;
+	}
+
+	public void setAppliedConfiguredActionOnFailure(
+			ConfiguredAction appliedConfiguredActionOnFailure) {
+		this.appliedConfiguredActionOnFailure = appliedConfiguredActionOnFailure;
+	}
+
+	public ConfiguredAction getAppliedConfiguredActionOnFailure() {
+		return appliedConfiguredActionOnFailure;
+	}
 }
