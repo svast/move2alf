09cc8efdd7c2d625146adcd7a50799aa0bc5c3a2
Dirk Ooms <dirk@onesparrow.com>
2010-12-09 16:25:03 +0000

    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@461 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/eu/xenit/move2alf/proprietary/vivium/action/LiPolisDocAction.java
+++ src/main/java/eu/xenit/move2alf/proprietary/vivium/action/LiPolisDocAction.java
@@ -1,3 +1,147 @@
+package eu.xenit.move2alf.proprietary.vivium.action;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import eu.xenit.move2alf.common.PropertyDefinition;
+import eu.xenit.move2alf.core.Action;
+import eu.xenit.poller.vivium.LiDocDescription;
+import eu.xenit.poller.vivium.LiGroupType;
+
+/*
+ * This parser is used for parsing the life polis docs
+ * polis docs is one group of documents and it has
+ * following properties:
+ * 
+ * common properties all life docs
+ * - JOBNAAM
+ * - DOCUMENT_ID
+ * - TYPE_DOCUMENT
+ * - RUBRIEK
+ * - NRVOLGKORG
+ * - DATUM
+ * 
+ * specific properties of polis docs
+ * - POLISNUMMER
+ * - PRODUCENTNR
+ * - NAAM_VN
+ * - NAAM_VZ
+ * - TAK
+ * - JAAR
+ * 
+ */
+
+public class LiPolisDocAction implements Action {
+	protected String name;
+	protected String description;
+	protected String className;
+	private Map<String,PropertyDefinition> configPropertyDefinitionMap = new HashMap<String,PropertyDefinition>();
+	private Map<String,PropertyDefinition> inputPropertyDefinitionMap = new HashMap<String,PropertyDefinition>();
+	private Map<String,PropertyDefinition> reportPropertyDefinitionMap = new HashMap<String, PropertyDefinition>();
+
+	public LiPolisDocAction(){
+		super();
+		this.setConfigPropertyDefinition("test",java.lang.String.class,null);
+		
+	}
+
+	@Override
+	public boolean execute(Map<String, Object> configPropertyMap, Map<String, Object> inputPropertyMap) {
+		//TODO
+		String keyFilePath = inputPropertyMap.get(key)
+		String keyfilePath = resultPath + File.separator + "keyfile.txt";
+		logger.debug("keyfilePath " + keyfilePath);
+		file = new File(keyfilePath);
+
+		logger.debug("Starting to process {}", keyfilePath);
+
+		// path where the .pdf files reside
+		String pdfPath = file.getParent();
+
+		FileReader fileReader = new FileReader(file);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy"); 
+		List<LiDocDescription> docDescriptionList = new ArrayList<LiDocDescription>();
+		LiDocDescription docDescription = null;
+
+		// cfr. keyfile.txt for formatting metadata
+
+		String line = null;
+		while ((line = bufferedReader.readLine()) != null) {
+			// ignore first line with field headers and last empty line
+			if (!line.startsWith("File Path") && line.trim().length() > 0) { 
+				// Use String.split instead of StringTokenizer because
+				// StringTokenizer ignores tokens without content.
+				//http://www.velocityreviews.com/forums/t136007-stringtokenizer-
+				// ignores-tokens-without-content.html
+
+				// If the lines ends with an empty field, the last field will
+				// only be picked up if we set the limit to 14
+				String[] lineFields = line.split("\\|", 14); 
+				// File Path
+				// String pdfFilePath = lineFields[0]; // not used, derived from
+				// documentID later on
+				// BEGIN_PDF|
+				// String beginPDF = lineFields[1]; // field not filled in by
+				// Adept
+				// JOBNAAM|
+				String jobName = lineFields[2];
+				// DOCUMENT_ID|
+				String pdfFileName = lineFields[3]+ ".pdf";
+				// TYPE_DOCUMENT|
+				String type = lineFields[4];
+				// POLISNUMMER|
+				String policyNumber = lineFields[5];
+				// PRODUCENTNUMMER|
+				String producerNumber = lineFields[6];
+				// RUBRIEK|
+				String categoryString = lineFields[7];
+				// DATUM|
+				String dateString = lineFields[8];
+				// TAK|
+				String contractTypeString = lineFields[9];
+				// JAAR|
+				String yearString = lineFields[10];
+				// NAAM_VN|
+				String nameInsuranceTaker = lineFields[11].replace("[", " ").replace("\\", "'");
+				// NAAM_VZ|
+				String nameInsured = lineFields[12].replace("[", " ").replace("\\", "'");
+				// NRVOLGKORG
+				String sequenceNumberString = lineFields[13];
+				
+				int sequenceNumber = 0;
+				try {
+					sequenceNumber = Integer.parseInt(sequenceNumberString);
+				} catch (NumberFormatException e) {
+					logger.error("Invalid sequenceNumber {}, skipping ...", sequenceNumberString);
+					continue;// go to next line
+				}
+
+				Date date = null;
+				try {
+					date = dateFormat.parse(dateString);
+				} catch (Exception e) {
+					logger.error("Can not parse date {} skipping ...", dateString);
+					continue;// go to next line
+				}
+
+				try {
+					Integer.parseInt(categoryString);
+				} catch (NumberFormatException e) {
+					logger.error("Invalid category {}, skipping ...", categoryString);
+					continue;// go to next line
+				}
+
+				// contractType can also contain char, e.g. 50R, so remove check
+=======
 //package eu.xenit.move2alf.proprietary.vivium.action;
 //
 //import java.io.BufferedReader;
@@ -126,6 +270,7 @@
 //					continue;// go to next line
 //				}
 //
+>>>>>>> .r454
 //				try {
 //					Integer.parseInt(categoryString);
 //				} catch (NumberFormatException e) {
