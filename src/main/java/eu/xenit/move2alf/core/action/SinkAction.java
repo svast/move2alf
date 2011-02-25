e36a0dc1d513b7019fa74b82361cefa7f59e2eec
Jonas Heylen <jheylen@xenit.eu>
2011-02-25 15:01:52 +0000

    Create SinkAction class
    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@707 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/main/java/eu/xenit/move2alf/core/action/SinkAction.java
@@ -0,0 +1,16 @@
+package eu.xenit.move2alf.core.action;
+
+import java.util.Map;
+
+import eu.xenit.move2alf.core.Action;
+import eu.xenit.move2alf.core.dto.ConfiguredAction;
+
+public class SinkAction extends Action {
+
+	@Override
+	protected void executeImpl(ConfiguredAction configuredAction,
+			Map<String, Object> parameterMap) {
+		
+	}
+
+}
