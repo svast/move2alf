bd0fd1f5d863b1330e8e4c8b9e48a5d00abde4e4
Ian Van Roosmalen <ivanroosmalen@xenit.eu>
2011-03-17 18:26:37 +0000

    Move documents action
    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@775 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/eu/xenit/move2alf/core/action/MoveDocumentsAction.java
+++ src/main/java/eu/xenit/move2alf/core/action/MoveDocumentsAction.java
@@ -8,16 +8,116 @@ import eu.xenit.move2alf.core.ConfigurableObject;
 import eu.xenit.move2alf.core.dto.ConfiguredAction;
 
 public class MoveDocumentsAction extends Action {
-
+	
 	@Override
 	protected void executeImpl(ConfiguredAction configuredAction,
 			Map<String, Object> parameterMap) {
 		// TODO move file in parametermap (key "file") to paths in parameters.
 		String moveBeforeProcessing = configuredAction.getParameter("moveBeforeProcessing");
-		File file = (File) parameterMap.get("file");
+		String moveAfterLoad = configuredAction.getParameter("moveAfterLoad");
+		String moveNotLoaded = configuredAction.getParameter("moveNotLoaded");
+		String inputFolder = configuredAction.getParameter("inputFolder");
 		
+		if("true".equals(moveBeforeProcessing)){
+			String moveDirectory = configuredAction.getParameter("moveBeforeProcessingPath");
+
+				File file = (File) parameterMap.get("file");
+				String absolutePath = file.getAbsolutePath();
+				
+				String relativePath = absolutePath.replace(inputFolder, "");
+				
+				String fullDestinationPath = moveDirectory+"/"+relativePath;
+				
+				boolean destinationPathExists = checkParentDirExists(fullDestinationPath);
+				
+				if(destinationPathExists){
+					boolean success = file.renameTo(new File(moveDirectory, file.getName()));
+					
+					if(!success){
+						//not successfully moved
+					}
+				}else{
+					//destination path could not be made
+				}
+
+		}
+		
+		if("true".equals(moveAfterLoad)){
+			if("true".equals(moveBeforeProcessing)){
+				inputFolder = configuredAction.getParameter("moveBeforeProcessingPath");
+			}
+			
+			String moveDirectory = configuredAction.getParameter("moveAfterLoadPath");
+			
+			File file = (File) parameterMap.get("file");
+			String absolutePath = file.getAbsolutePath();
+			
+			String relativePath = absolutePath.replace(inputFolder, "");
+			
+			String fullDestinationPath = moveDirectory+"/"+relativePath;
+			
+			boolean destinationPathExists = checkParentDirExists(fullDestinationPath);
+			
+			if(destinationPathExists){
+				boolean success = file.renameTo(new File(moveDirectory, file.getName()));
+				
+				if(!success){
+					//not successfully moved
+				}
+			}else{
+				//destination path could not be made
+			}
+		}
+		
+		if("true".equals(moveNotLoaded)){
+			if("true".equals(moveBeforeProcessing)){
+				inputFolder = configuredAction.getParameter("moveBeforeProcessingPath");
+			}
+			
+			String moveDirectory  = configuredAction.getParameter("moveNotLoadedPath");
+			
+			File file = (File) parameterMap.get("file");
+			String absolutePath = file.getAbsolutePath();
+			
+			String relativePath = absolutePath.replace(inputFolder, "");
+			
+			String fullDestinationPath = moveDirectory+"/"+relativePath;
+			
+			boolean destinationPathExists = checkParentDirExists(fullDestinationPath);
+			
+			if(destinationPathExists){
+				boolean success = file.renameTo(new File(moveDirectory, file.getName()));
+				
+				if(!success){
+					//not successfully moved
+				}
+			}else{
+				//destination path could not be made
+			}
+		}
 	}
 
+	public boolean checkParentDirExists(String path){
+		
+		int index = path.lastIndexOf("/");
+		String newPath = path.substring(0, index-1);
+		
+		File parentDestination = new File(newPath);
+		boolean exists = parentDestination.exists();
+		
+		if(!exists){
+			boolean destinationPathMade = checkParentDirExists(newPath);
+			if(destinationPathMade){
+				boolean success = (new File(newPath)).mkdir();
+				return success;
+			}else{
+				return false;	
+			}
+		}
+		
+		return true;
+	}
+	
 	@Override
 	public String getCategory() {
 		return ConfigurableObject.CAT_DEFAULT;
--- src/main/java/eu/xenit/move2alf/core/action/TransformAction.java
+++
@@ -1,5 +0,0 @@
-package eu.xenit.move2alf.core.action;
-
-public class TransformAction {
-
-}
