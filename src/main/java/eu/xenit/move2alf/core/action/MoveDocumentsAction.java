868f52cb26d8827a0e1340b319630cfeeb71ca33
Jonas Heylen <jheylen@xenit.eu>
2011-03-17 15:53:58 +0000

    MoveDocumentsAction example
    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@770 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/eu/xenit/move2alf/core/action/MoveDocumentsAction.java
+++ src/main/java/eu/xenit/move2alf/core/action/MoveDocumentsAction.java
@@ -1,5 +1,38 @@
 package eu.xenit.move2alf.core.action;
 
-public class MoveDocumentsAction {
+import java.io.File;
+import java.util.Map;
+
+import eu.xenit.move2alf.core.Action;
+import eu.xenit.move2alf.core.ConfigurableObject;
+import eu.xenit.move2alf.core.dto.ConfiguredAction;
+
+public class MoveDocumentsAction extends Action {
+
+	@Override
+	protected void executeImpl(ConfiguredAction configuredAction,
+			Map<String, Object> parameterMap) {
+		// TODO move file in parametermap (key "file") to paths in parameters.
+		String moveBeforeProcessing = configuredAction.getParameter("moveBeforeProcessing");
+		File file = (File) parameterMap.get("file");
+		
+	}
+
+	@Override
+	public String getCategory() {
+		return ConfigurableObject.CAT_DEFAULT;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
