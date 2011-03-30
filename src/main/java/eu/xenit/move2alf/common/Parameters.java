d61f8500b742aa68d3dfe870373aeafb0002ccd1
Jonas Heylen <jheylen@xenit.eu>
2011-03-30 15:52:35 +0000

    Parameters.java
    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@832 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/main/java/eu/xenit/move2alf/common/Parameters.java
@@ -0,0 +1,56 @@
+package eu.xenit.move2alf.common;
+
+public class Parameters {
+
+	public static final String PARAM_PATH = "path";
+
+	public static final String PARAM_RECURSIVE = "recursive";
+	
+	public static final String PARAM_COUNTER = "counter";
+	
+	public static final String PARAM_CYCLE = "cycle";
+	
+	public static final String PARAM_THREADPOOL = "threadpool";
+	
+	public static final String PARAM_THREAD = "thread";
+
+	public static final String PARAM_FILE = "file";
+	
+	public static final String PARAM_RELATIVE_PATH = "relativePath";
+	
+	public static final String PARAM_METADATA = "metadata";
+	
+	public static final String PARAM_MULTI_VALUE_METADATA = "multiValueMetadata";
+
+	public static final String PARAM_DESCRIPTION = "description";
+	
+	public static final String PARAM_CONTENTTYPE = "contenttype";
+	
+	public static final String PARAM_NAMESPACE = "namespace";
+	
+	public static final String PARAM_MIMETYPE = "mimetype";
+
+	public static final String PARAM_ACL = "acl";
+	
+	public static final String PARAM_INHERIT_PERMISSIONS = "inheritPermissions";
+	
+	public static final String PARAM_STATUS = "status";
+	public static final String VALUE_FAILED = "failed";
+	public static final String VALUE_OK = "ok";
+
+	public static final String PARAM_ERROR_MESSAGE = "errormessage";
+
+	public static final String PARAM_MOVE_AFTER_LOAD = "moveAfterLoad";
+	public static final String PARAM_MOVE_AFTER_LOAD_PATH = "moveAfterLoadPath";
+
+	public static final String PARAM_MOVE_BEFORE_PROCESSING = "moveBeforeProcessing";
+	public static final String PARAM_MOVE_BEFORE_PROCESSING_PATH = "moveBeforeProcessingPath";
+
+	public static final String PARAM_MOVE_NOT_LOADED = "moveNotLoaded";
+	public static final String PARAM_MOVE_NOT_LOADED_PATH = "moveNotLoadedPath";	
+
+	public static final String PARAM_STAGE = "stage";
+	public static final String VALUE_AFTER = "after";
+	public static final String VALUE_BEFORE = "before";
+
+}
