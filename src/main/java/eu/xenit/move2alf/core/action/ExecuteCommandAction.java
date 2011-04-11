3e3ef6e1863079a93ff2fd2175a8a54df019abca
Ian Van Roosmalen <ivanroosmalen@xenit.eu>
2011-04-11 07:13:20 +0000

    git-svn-id: https://xenitsupport.jira.com/svn/XENMTOA/trunk/move2alf-war@905 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/main/java/eu/xenit/move2alf/core/action/ExecuteCommandAction.java
@@ -0,0 +1,92 @@
+package eu.xenit.move2alf.core.action;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.xenit.move2alf.common.Parameters;
+import eu.xenit.move2alf.core.Action;
+import eu.xenit.move2alf.core.ConfigurableObject;
+import eu.xenit.move2alf.core.dto.ConfiguredAction;
+
+public class ExecuteCommandAction extends Action{
+
+	private static final Logger logger = LoggerFactory
+	.getLogger(ExecuteCommandAction.class);
+	
+	@Override
+	public void execute(ConfiguredAction configuredAction,
+			Map<String, Object> parameterMap){
+
+		String command = configuredAction
+					.getParameter(Parameters.PARAM_COMMAND);
+
+		logger.debug("Executing command "+ command);
+		
+		ProcessBuilder pb = new ProcessBuilder(command);
+		pb.redirectErrorStream(true);
+		Process process = null;
+		try {
+			process = pb.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		InputStream is = process.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		
+		try {
+			while ((line = br.readLine()) != null) {
+				logger.debug(line);
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			process.waitFor();
+		} catch (InterruptedException ie) {
+			logger.error("Problem running command");
+		}
+		
+		logger.info("Command finished");	
+
+	}
+	
+	@Override
+	protected void executeImpl(ConfiguredAction configuredAction,
+			Map<String, Object> parameterMap) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
