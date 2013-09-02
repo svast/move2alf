344f2ad7820ac7a5370873ebc43a704340c9706b
Roxana Angheluta <roxana.angheluta@xenit.eu>
2013-09-02 15:55:52 +0200

    Added smtp authentication (needed if Move2Alf runs on an Amazon instance).

---
+++ move2alf-war/src/main/java/eu/xenit/move2alf/common/Mappings.java
@@ -0,0 +1,11 @@
+package eu.xenit.move2alf.common;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rox
+ * Date: 9/2/13
+ * Time: 1:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Mappings {
+}
