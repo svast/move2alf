161d3bd725a3f664adbc6a849f3b11fef62389e9
Laurent Van der Linden <laurent.vanderlinden@xenit.eu>
2013-09-25 16:55:42 +0200

    XENMTOA-213 draft

--- move2alf-war/src/main/java/eu/xenit/move2alf/core/action/ActionClassInfoService.java
+++ move2alf-war/src/main/java/eu/xenit/move2alf/core/action/ActionClassInfoService.java
@@ -2,10 +2,6 @@ package eu.xenit.move2alf.core.action;
 
 import eu.xenit.move2alf.core.AbstractClassInfoService;
 import eu.xenit.move2alf.pipeline.actions.Action;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.filter.AssignableTypeFilter;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -18,21 +14,13 @@ import javax.annotation.PostConstruct;
  */
 @Service
 public class ActionClassInfoService extends AbstractClassInfoService {
-
-    private static final Logger logger = LoggerFactory.getLogger(ActionClassInfoService.class);
-    public ActionClassInfoService(){
-
-    }
-
-
-    @Override
-    protected void addFilters(ClassPathScanningCandidateComponentProvider provider) {
-        provider.addIncludeFilter(new AssignableTypeFilter(Action.class));
-    }
-
     @PostConstruct
     public void init() {
-        scanForClasses("eu.xenit");
+      scanForClasses("eu.xenit");
     }
 
+    @Override
+    protected Class<?> getTargetType() {
+        return Action.class;
+    }
 }
