b4e0b6375f708ada542aa37fe88d7f2a4017c069
Thijs Lemmens <thijs.lemmens@xenit.eu>
2013-09-26 10:37:18 +0200

    Added Parameterized and used it in the ObjectFactory

--- move2alf-war/src/main/scala/eu/xenit/move2alf/logic/ObjectFactory.scala
+++ move2alf-war/src/main/scala/eu/xenit/move2alf/logic/ObjectFactory.scala
@@ -3,6 +3,7 @@ package eu.xenit.move2alf.logic
 import scala.collection.JavaConversions._
 import eu.xenit.move2alf.common.LogHelper
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory
+import eu.xenit.move2alf.core.action.Parameterized
 
 /**
  * User: Thijs Lemmens (tlemmens@xenit.eu)
@@ -16,36 +17,42 @@ class ObjectFactory[T](clazz: Class[_], parameters: java.util.Map[String, String
     val basicAction: T = constructor.newInstance().asInstanceOf[T]
       logger.error("autowiring " + clazz)
     beanFactory.autowireBean(basicAction)
-    val methods = clazz.getMethods
-    val methodMap = methods map {
-      method => (method.getName, method)
-    } toMap
 
-    parameters foreach {
-      case (key, value) => {
-        try {
-          val method = methodMap("set"+key.capitalize)
+    basicAction match {
+      case b: Parameterized => b.setParameters(parameters)
+      case _ => {
+        val methods = clazz.getMethods
+        val methodMap = methods map {
+          method => (method.getName, method)
+        } toMap
 
-          try {
-            logger.debug("Setting parameter: "+key+", value: "+value.toString+" for clazz=" + clazz)
-            method.invoke(basicAction, value)
-          } catch {
-            case e: IllegalArgumentException => {
-              logger.error("Could not set parameter: "+key+"\n" +
-                "Method parameter: "+method.getParameterTypes()(0).getCanonicalName+"\n" +
-                "Value type: "+value.getClass.getCanonicalName, e)
-            }
-            case e: NullPointerException => {
-              logger.error("NullPointer", e)
-              if(method == null){
-                logger.error("method is null")
+        parameters foreach {
+          case (key, value) => {
+            try {
+              val method = methodMap("set"+key.capitalize)
+
+              try {
+                logger.debug("Setting parameter: "+key+", value: "+value.toString+" for clazz=" + clazz)
+                method.invoke(basicAction, value)
+              } catch {
+                case e: IllegalArgumentException => {
+                  logger.error("Could not set parameter: "+key+"\n" +
+                    "Method parameter: "+method.getParameterTypes()(0).getCanonicalName+"\n" +
+                    "Value type: "+value.getClass.getCanonicalName, e)
+                }
+                case e: NullPointerException => {
+                  logger.error("NullPointer", e)
+                  if(method == null){
+                    logger.error("method is null")
+                  }
+                  logger.error("key: "+key)
+                  logger.error("value: "+value)
+                }
               }
-              logger.error("key: "+key)
-              logger.error("value: "+value)
+            } catch {
+              case e: NoSuchElementException => logger.info("No setter for parameter: "+key)
             }
           }
-        } catch {
-          case e: NoSuchElementException => logger.info("No setter for parameter: "+key)
         }
       }
     }
