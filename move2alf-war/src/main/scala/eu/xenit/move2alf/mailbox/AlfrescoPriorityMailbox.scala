9c73fcdd3209a03e8a1e6adc5cab347d5fe34105
thijs <tlemmens@xenit.eu>
2014-02-24 18:48:48 +0100

    Fix actors dying too soon

--- move2alf-war/src/main/scala/eu/xenit/move2alf/mailbox/AlfrescoPriorityMailbox.scala
+++ move2alf-war/src/main/scala/eu/xenit/move2alf/mailbox/AlfrescoPriorityMailbox.scala
@@ -4,6 +4,8 @@ import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
 import akka.actor.ActorSystem
 import com.typesafe.config.Config
 import eu.xenit.move2alf.core.action.messages._
+import eu.xenit.move2alf.pipeline.{EOC, M2AMessage}
+import eu.xenit.move2alf.pipeline.actors.{ReadyToDie, Negotiate, Flush}
 
 /**
  * User: Thijs Lemmens (tlemmens@xenit.eu)
@@ -12,12 +14,22 @@ import eu.xenit.move2alf.core.action.messages._
  */
 class AlfrescoPriorityMailbox(settings: ActorSystem.Settings, config: Config) extends UnboundedPriorityMailbox(
   PriorityGenerator {
-    case m:SendBatchMessage => 0
-    case m:DeleteMessage => 0
-    case m:BatchACLMessage => 0
-    case m:ListMessage => 0
-    case m:CheckExistenceMessage => 0
+    case M2AMessage(m) => {
+      m match {
+        case m:SendBatchMessage => 0
+        case m:DeleteMessage => 0
+        case m:BatchACLMessage => 0
+        case m:ListMessage => 0
+        case m:CheckExistenceMessage => 0
+        case _ => 1
+      }
+    }
+    case Negotiate(_) => 2
+    case Flush(_) => 3
+    case ReadyToDie => 4
+    case EOC => 5
     case _ => 1
+
   }
 )
 
