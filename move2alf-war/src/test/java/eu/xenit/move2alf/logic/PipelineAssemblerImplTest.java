136e784225a532664ac4f5bf126af8500c1a5ae5
Roxana Angheluta <roxana.angheluta@xenit.eu>
2014-02-24 13:53:47 +0100

    Compilation problem.

--- move2alf-war/src/test/java/eu/xenit/move2alf/logic/PipelineAssemblerImplTest.java
+++ move2alf-war/src/test/java/eu/xenit/move2alf/logic/PipelineAssemblerImplTest.java
@@ -24,66 +24,51 @@ public class PipelineAssemblerImplTest {
         ConfiguredAction start = new ConfiguredAction();
         start.setActionId(PipelineAssemblerImpl.START);
 
-        ConfiguredAction end = start;
-
         ConfiguredAction validateAction = new ConfiguredAction();
         validateAction.setActionId(PipelineAssemblerImpl.VALIDATE_DESTINATION);
         validateAction.addReceiver(PipelineAssemblerImpl.REPORTER, reporter);
-        end.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, validateAction);
-        end = validateAction;
+        start.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, validateAction);
 
         ConfiguredAction sourceAction = new ConfiguredAction();
         sourceAction.setActionId(PipelineAssemblerImpl.SOURCE_CMIS_ID);
-        sourceAction.setNmbOfWorkers(1);
         sourceAction.addReceiver(PipelineAssemblerImpl.REPORTER, reporter);
-        end.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, sourceAction);
+        validateAction.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, sourceAction);
 
-
+        // a loop from cmisRecursive to cmisQuery and back
         ConfiguredAction cmisRecursive = new ConfiguredAction();
         cmisRecursive.addReceiver(PipelineAssemblerImpl.REPORTER, reporter);
-        end.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, cmisRecursive);
-        end = cmisRecursive;
+        sourceAction.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, cmisRecursive);
+
 
         ConfiguredAction cmisQuery = new ConfiguredAction();
         cmisQuery.setActionId(PipelineAssemblerImpl.CMIS_QUERY);
         cmisQuery.addReceiver(PipelineAssemblerImpl.REPORTER, reporter);
         cmisQuery.addReceiver(PipelineAssemblerImpl.RECURSIVE_CMIS, cmisRecursive);
-        end.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, cmisQuery);
-        end = cmisQuery;
+        sourceAction.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, cmisQuery);
 
         cmisRecursive.addReceiver(PipelineAssemblerImpl.RECURSIVE_RECEIVER, cmisQuery);
 
         ConfiguredAction metadataAction = new ConfiguredAction();
         metadataAction.setActionId(PipelineAssemblerImpl.METADATA_ACTION_ID);
         metadataAction.addReceiver(PipelineAssemblerImpl.REPORTER, reporter);
-        end.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, metadataAction);
-        end = metadataAction;
+        cmisQuery.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, metadataAction);
 
-        /*onfiguredAction listAction;
-
-            listAction = new ConfiguredAction();
-            listAction.setActionId(EXISTENCE_CHECK_ID);
-        listAction.setNmbOfWorkers(1);
-        listAction.setParameter(ListAction$.MODULE$.PARAM_PATH(), jobModel.getDestinationFolder());
-        listAction.setParameter(ActionWithDestination$.MODULE$.PARAM_DESTINATION(), String.valueOf(jobModel.getDest()));
-        listAction.addReceiver(REPORTER, reporter);
-        end.addReceiver(DEFAULT_RECEIVER, listAction);
-        end = listAction;
+        ConfiguredAction listAction = new ConfiguredAction();
+        listAction.addReceiver(PipelineAssemblerImpl.REPORTER, reporter);
+        metadataAction.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, listAction);
 
         ConfiguredAction endAction = new ConfiguredAction();
-        endAction.setActionId(END_ACTION);
-        endAction.setClassId(actionClassService.getClassId(M2AlfEndAction.class));
-        endAction.setNmbOfWorkers(1);
-        endAction.setParameter(M2AlfEndAction.PARAM_SENDREPORT, Boolean.toString(jobModel.getSendReport()));
-        endAction.setParameter(M2AlfEndAction.PARAM_REPORT_TO, jobModel.getSendReportText());
-        endAction.setParameter(M2AlfEndAction.PARAM_SENDERROR, Boolean.toString(jobModel.getSendNotification()));
-        endAction.setParameter(M2AlfEndAction.PARAM_ERROR_TO, jobModel.getSendNotificationText());
-        end.addReceiver(DEFAULT_RECEIVER, endAction);*/
-        return null;
+        endAction.setActionId(PipelineAssemblerImpl.END_ACTION);
+        listAction.addReceiver(PipelineAssemblerImpl.DEFAULT_RECEIVER, endAction);
+
+        return start;
     }
 
     void getActionConfigTest(){
         PipelineAssemblerImpl pipeline = new PipelineAssemblerImpl();
 
+        ConfiguredAction start = mockConfiguredAction();
+
+        pipeline.getActionConfig(start);
     }
 }
