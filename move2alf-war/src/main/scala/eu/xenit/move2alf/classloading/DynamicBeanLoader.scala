2c40a22baec6d9c2ad677ac6a241d417ca0b4055
Roxana Angheluta <roxana.angheluta@xenit.eu>
2015-06-01 08:42:55 +0200

    Added flag: type.server (development/production), for the dynamic loading of classes. i

--- move2alf-war/src/main/scala/eu/xenit/move2alf/classloading/DynamicBeanLoader.scala
+++ move2alf-war/src/main/scala/eu/xenit/move2alf/classloading/DynamicBeanLoader.scala
@@ -10,45 +10,51 @@ import scala.collection.JavaConversions._
 import java.util.{Set => JSet}
 import org.springframework.beans.factory.FactoryBean
 import org.springframework.stereotype.Service
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.{Autowired, Value}
 import java.util
 import org.springframework.util.StringUtils
 
 /**
  * @author Laurent Van der Linden
  */
-
 trait ClasspathScanner {
-    def findMatchingClasses(basePackage: String, resourceType: Class[_],
-                            parentClassLoader: ClassLoader = classOf[Action].getClassLoader): JSet[Class[_]]
+  val SERVER_DEVELOPMENT: String = "development"
+  val SERVER_PRODUCTION: String = "production"
 
-    def isStatic: Boolean
+  def findMatchingClasses(basePackage: String, resourceType: Class[_],
+                          parentClassLoader: ClassLoader = classOf[Action].getClassLoader): JSet[Class[_]]
+
+  def isStatic: Boolean
 }
+
 
 class SystemClasspathScanner extends ClasspathScanner {
 
-    def findMatchingClasses(basePackage: String, resourceType: Class[_], parentClassLoader: ClassLoader): JSet[Class[_]] = {
-        val provider = new ClassPathScanningCandidateComponentProvider(false)
-        provider.addIncludeFilter(new AssignableTypeFilter(resourceType))
-        provider.findCandidateComponents(basePackage).map(bd => parentClassLoader.loadClass(bd.getBeanClassName))
-    }
+  def findMatchingClasses(basePackage: String, resourceType: Class[_], parentClassLoader: ClassLoader): JSet[Class[_]] = {
+    val provider = new ClassPathScanningCandidateComponentProvider(false)
+    provider.addIncludeFilter(new AssignableTypeFilter(resourceType))
+    provider.findCandidateComponents(basePackage).map(bd => parentClassLoader.loadClass(bd.getBeanClassName))
+  }
 
-    def isStatic: Boolean = true
+  def isStatic: Boolean = true
 }
 
-class DynamicClasspathScanner(var classPaths: Array[String]) extends ClasspathScanner {
-    val systemClasspathScanner = new SystemClasspathScanner()
+class DynamicClasspathScanner(var classPaths: Array[String], var typeServer: String) extends ClasspathScanner {
+  val systemClasspathScanner = new SystemClasspathScanner()
 
-    def findMatchingClasses(basePackage: String, resourceType: Class[_], parentClassLoader: ClassLoader): JSet[Class[_]] = {
-        val cl = new LocalUrlClassLoader(classPaths.map(new URL(_)), parentClassLoader)
+  def findMatchingClasses(basePackage: String, resourceType: Class[_], parentClassLoader: ClassLoader): JSet[Class[_]] = {
+    val cl = new LocalUrlClassLoader(classPaths.map(new URL(_)), parentClassLoader)
 
-        val provider = new ClassPathScanningCandidateComponentProvider(false)
-        provider.setResourceLoader(new DefaultResourceLoader(cl))
-        provider.addIncludeFilter(new AssignableTypeFilter(resourceType))
-        provider.findCandidateComponents("").map(bd => cl.loadClass(bd.getBeanClassName))
-    }
+    val provider = new ClassPathScanningCandidateComponentProvider(false)
+    provider.setResourceLoader(new DefaultResourceLoader(cl))
+    provider.addIncludeFilter(new AssignableTypeFilter(resourceType))
+    provider.findCandidateComponents("").map(bd => cl.loadClass(bd.getBeanClassName))
+  }
 
-    def isStatic: Boolean = false
+  def isStatic: Boolean = {
+    if (StringUtils.hasText(typeServer) && typeServer.equals(SERVER_PRODUCTION)) true
+    else false
+  }
 }
 
 /**
@@ -56,48 +62,52 @@ class DynamicClasspathScanner(var classPaths: Array[String]) extends ClasspathSc
  * We only load user classes from the hot deploy path
  * @param classPaths
  */
-class MixedClasspathScanner(var classPaths: Array[String]) extends ClasspathScanner {
-    val systemClasspathScanner = new SystemClasspathScanner()
-    val dynamicClasspathScanner = new DynamicClasspathScanner(classPaths)
+class MixedClasspathScanner(var classPaths: Array[String], var typeServer: String) extends ClasspathScanner {
+  val systemClasspathScanner = new SystemClasspathScanner()
+  val dynamicClasspathScanner = new DynamicClasspathScanner(classPaths, typeServer)
 
-    def findMatchingClasses(basePackage: String, resourceType: Class[_], parentClassLoader: ClassLoader): JSet[Class[_]] = {
-        val result = new util.HashSet[Class[_]]()
-        val canonicalNames = new util.HashSet[String]()
+  def findMatchingClasses(basePackage: String, resourceType: Class[_], parentClassLoader: ClassLoader): JSet[Class[_]] = {
+    val result = new util.HashSet[Class[_]]()
+    val canonicalNames = new util.HashSet[String]()
 
-        // we want to give preference to the dynamic classloader
-        // also compare by name as A.class does not equal B.class if loaded by different classloader
-        val prober: (Class[_]) => AnyVal = {
-            clazz =>
-                val cName: String = clazz.getCanonicalName
-                if (!canonicalNames.contains(cName)) {
-                    canonicalNames.add(cName)
-                    result.add(clazz)
-                }
+    // we want to give preference to the dynamic classloader
+    // also compare by name as A.class does not equal B.class if loaded by different classloader
+    val prober: (Class[_]) => AnyVal = {
+      clazz =>
+        val cName: String = clazz.getCanonicalName
+        if (!canonicalNames.contains(cName)) {
+          canonicalNames.add(cName)
+          result.add(clazz)
         }
-
-        dynamicClasspathScanner.findMatchingClasses(basePackage, resourceType).foreach (prober)
-
-        systemClasspathScanner.findMatchingClasses(basePackage, resourceType).foreach(prober)
-
-        result
     }
 
-    def isStatic: Boolean = false
+    dynamicClasspathScanner.findMatchingClasses(basePackage, resourceType).foreach (prober)
+
+    systemClasspathScanner.findMatchingClasses(basePackage, resourceType).foreach(prober)
+
+    result
+  }
+
+  def isStatic: Boolean = {
+    if (StringUtils.hasText(typeServer) && typeServer.equals(SERVER_PRODUCTION)) true
+    else false
+  }
 }
 
 @Service
 class ClasspathScannerFactory extends FactoryBean[ClasspathScanner] {
-    @Value(value = "#{'${hotdeploy.paths:}'}") private var hotdeployPaths: String = null
+  @Value(value = "#{'${hotdeploy.paths:}'}") private var hotdeployPaths: String = null
+  @Value(value = "#{'${type.server:}'}") private var typeServer: String = null
 
-    def getObject: ClasspathScanner = {
-        if (StringUtils.hasText(hotdeployPaths) ) {
-            new MixedClasspathScanner(hotdeployPaths.split(","))
-        } else {
-            new SystemClasspathScanner()
-        }
+  def getObject: ClasspathScanner = {
+    if (StringUtils.hasText(hotdeployPaths)) {
+      new MixedClasspathScanner(hotdeployPaths.split(","),typeServer)
+    } else {
+      new SystemClasspathScanner()
     }
+  }
 
-    def getObjectType = classOf[ClasspathScanner]
+  def getObjectType = classOf[ClasspathScanner]
 
-    def isSingleton = true
+  def isSingleton = true
 }
